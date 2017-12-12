dabbfe27168f4958ea7f93dfd3c9c5ccc34f2f8e
Snake73457345 <axelbauduin@gmail.com>
2017-12-12 20:02:49 +0100

    Mise a jour 1
    Mise a jour 1

---
+++ 041217serie3/Exercice 3 (en cours)/Correction/page3.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 12/12/17
+ * Time: 17:51
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<?php
+$soustitre = "Résultat";
+require_once "head.php" ;
+?>
+<?php require_once "head.php" ; ?>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="Page1.php">Mon Portfolio</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="Page1.php">Home</a></li>
+            <li><a href="Page2.php">Me contacter</a></li>
+            <li class="active"><a href="Page3.php">Resultat</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <header id="header" class="col-lg-10 offset-3">
+            <h1>Résultat des boucles:</h1>
+        </header>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <section class="col-lg-10">
+            <div class="row">
+                <article class="col-sm-4">
+
+                    <h2>Boucle ceci est une ligne + numéro</h2>
+                    <p>Exercice 1</p>
+                    <?php
+                    for ($cpt=0;$cpt <= 10; $cpt++)
+                    {
+                        echo "Ceci est la ligne n°".$cpt;
+                        echo "<br/>";
+                    }
+                    ?>
+
+
+                </article>
+                <article class="col-sm-4">
+
+                    <h2>Boucle ceci est une ligne + numéro (liste a puce)</h2>
+                    <p>Exercice 2</p>
+                    <?php
+
+                    for ($cpt=0;$cpt <= 10; $cpt++)
+                    {
+
+                        echo '<ul><li>'."Ceci est la ligne n°".$cpt.'</li></ul>';
+                    }
+                    ?>
+                </article>
+                <article class="col-sm-4">
+
+                    <h2>Freelancer</h2>
+                    <p>Thème freelancer</p>
+                </article>
+                <article class="col-sm-4">
+
+                    <h2>Grayscale</h2>
+                    <p>Thème multifonctions</p>
+                </article>
+
+
+            </div>
+        </section>
+        <aside class="col-lg-2">
+            <p>Contenu publicitaire</p>
+        </aside>
+    </div>
+</div>
+</body>
+</html>
---
+++ 271117serie1et2/Correction/S1 Exercice 2 et S2 Exercice 1 (fini)/page1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<?php
+$soustitre = "Page d'accueil";
+require_once "head.php" ;
+?>
+<?php require_once "head.php" ; ?>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="Page1.php">Mon Portfolio</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="Page1.php">Home</a></li>
+				<li><a href="Page2.php">Me contacter</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container-fluid">
+		<div class="row">
+			<header id="header" class="col-lg-10 offset-3">
+				<h1>Portfolio:</h1>
+				<p>Exemples de portfolios</p>
+			</header>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<section class="col-lg-10">
+				<div class="row">
+					<article class="col-sm-4">
+						<img class="img-responsive" src="https://startbootstrap.com/assets/img/templates/creative.jpg" alt="">
+						<h2>Creative</h2>
+						<p>Theme créatif One Page</p>
+						
+					</article>
+					<article class="col-sm-4">
+						<img class="img-responsive" src="https://startbootstrap.com/assets/img/templates/agency.jpg" alt="">
+						<h2>Agency</h2>
+						<p>Thème professionel</p>
+					</article>
+					<article class="col-sm-4">
+						<img class="img-responsive" src="https://startbootstrap.com/assets/img/templates/freelancer.jpg" alt="">
+						<h2>Freelancer</h2>
+						<p>Thème freelancer</p>
+					</article>
+					<article class="col-sm-4">
+						<img class="img-responsive" src="https://startbootstrap.com/assets/img/templates/grayscale.jpg" alt="">
+						<h2>Grayscale</h2>
+						<p>Thème multifonctions</p>
+					</article>
+					
+					
+				</div>
+			</section>
+			<aside class="col-lg-2">
+				<p>Contenu publicitaire</p>
+			</aside>
+		</div>
+	</div>
+</body>
+</html>

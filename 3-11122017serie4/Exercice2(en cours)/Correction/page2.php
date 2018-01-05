0e720a679542ebabd69fbf608b2b196636cecdcc
Snake7345 <thebasketman7@hotmail.com>
2018-01-05 17:06:40 +0100

    Mise a jour 3.2.1
    Mise a jour des dossiers

---
+++ 3-11122017serie4/Exercice2(en cours)/Correction/page2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+$soustitre = "Me trouver";
+require_once "head.php" ;
+?>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="Page1.php">Mon Portfolio</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="Page1.php">Home</a></li>
+				<li class="active"><a href="Page2.php">Me contacter</a></li>
+                <li><a href="Page3.php">Resultat</a></li>
+                <li><a href="Page4.php">Tableaux</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container-fluid">
+		<div class="row">
+			<header id="header" class="col-lg-10 offset-3">
+				<h1>Me trouver</h1>
+			</header>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<section class="col-lg-10">
+				<div class="row">
+					<article class="col-lg-8">
+						
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2542.0927628423865!2d4.4572167159460605!3d50.420743297385734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2261bd787a80f%3A0x68cd1e16a7f40621!2sColl%C3%A8ge+Technique+des+Aum%C3%B4niers+du+Travail!5e0!3m2!1sfr!2sbe!4v1510522584240" width="600" height="450"   allowfullscreen></iframe>
+						
+					</article>
+					<article class="col-sm-4">
+						<h2>Adresse</h2>
+						<img class="img-responsive" src="http://www.shantee.net/wp-content/uploads/2016/06/google-maps.jpg" alt="">
+						<p>Collège Technique "Aumôniers du Travail" <br>
+							Enseignement de Promotion Sociale <br>
+							Grand rue 185 <br>
+							6000 Charleroi
+						</p>
+					</article>
+					<article class="col-sm-4">
+						<h2>Me contacter</h2>
+						<img class="img-responsive" src="images/logo-atc.png" alt="">
+						<p class="glyphicon glyphicon-earphone">071/285.905</p>
+					</article>
+				</div>
+			</section>
+			<aside class="col-lg-2">
+				<p>Contenu publicitaire</p>
+			</aside>
+		</div>
+	</div>
+</body>
+</html>
---
+++ 3-11122017serie4/Exercice2(en cours)/Correction/page4.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theba
+ * Date: 31/12/17
+ * Time: 03:13
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<?php
+$soustitre = "Résultat";
+require_once "head.php" ;
+?>
+<?php require_once "head.php" ; ?>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="Page1.php">Mon Portfolio</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="Page1.php">Home</a></li>
+            <li><a href="Page2.php">Me contacter</a></li>
+            <li><a href="Page3.php">Resultat</a></li>
+            <li class="active"><a href="Page4.php">Tableaux</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <header id="header" class="col-lg-10 offset-3">
+            <h1>Les tableaux en PHP:</h1>
+        </header>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <section class="col-lg-10">
+            <div class="row">
+                <article class="col-sm-4">
+
+                    <h2>Tableau indicé 1</h2>
+                    <p>Exercice 1</p>
+                    <?php
+
+
+                    // $NbrLigne : le nombre de lignes
+                    // $NbrCol : le nombre de colonnes : calcul automatique
+                    // --------------------------------
+                    // (exemple)
+                    $NbrLigne = 7;
+                    $jourdelasemaine = array('Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche');
+                    // --------------------------------
+                    // $NbreData : le nombre de données à afficher
+                    $NbreData = count($jourdelasemaine);
+                    // --------------------------------
+                    $NbrCol = 0;
+                    // affichage
+                    if ($NbreData != 0)
+                    {
+                        $cpt1 = 0; // indice du tableau
+                        ?>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <?php
+                                while ($cpt1 < $NbreData)
+                                {
+                                    ?>
+                                    <td valign="top">
+                                    <table border>
+                                    <?php
+                                    for ($cpt2=1; $cpt2<=$NbrLigne; $cpt2++)
+                                     {
+                                        if ($cpt1 < $NbreData)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td>
+                                                    <?php
+                                                    // -------------------------
+                                                    // DONNEES A AFFICHER dans la cellule
+                                                    echo $jourdelasemaine[$cpt1];
+                                                    // -------------------------
+                                                    ?>
+                                                </td>
+                                            </tr>
+                                            <?php
+                                            $cpt1++;
+                                        }
+                                        if ($cpt2 == $NbrLigne)
+                                        {
+                                            ?>
+                                            </table>
+                                            </td>
+                                            <?php
+                                        }
+                                    }
+                                    $NbrCol++;
+                                }
+                                ?>
+                            </tr>
+                            <tbody>
+                        </table>
+                        <?php
+                    }
+                    else
+                        {
+                        ?>	pas de données à afficher
+                        <?php
+                        }
+                    ?>
+                </article>
+
+                <article class="col-sm-4">
+
+                    <h2>Tableau associatif</h2>
+                    <p>Exercice 2</p>
+
+
+
+                </article>
+
+                <article class="col-sm-4">
+
+                    <h2>tableau indicé 52</h2>
+                    <p>Exercice 3</p>
+
+
+
+                </article>
+            </div>
+        </section>
+        <aside class="col-lg-2">
+            <p>Contenu publicitaire</p>
+        </aside>
+    </div>
+</div>
+</body>
+</html>

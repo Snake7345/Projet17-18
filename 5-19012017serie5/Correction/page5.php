2d3247504a80110d35517c591ee4fac30b7190d1
Snake7345 <thebasketman7@hotmail.com>
2018-01-25 01:54:35 +0100

    Correction mineur 3.2.3.7
    Correction mineur du alert

--- 5-19012017serie5/Correction/page5.php
+++ 5-19012017serie5/Correction/page5.php
@@ -32,7 +32,7 @@ require "head.php" ;
         ";
 ?>
         <?php if(array_key_exists('errors',$_SESSION)) : ?>
-            <div class="alert" alert-danger">
+            <div class="alert alert-danger">
                 <?= implode('<br>', $_SESSION['errors']) ?>
             </div>
         <?php unset($_SESSION['errors']); endif; ?>

533c2340d3ec8ea42c5f46e34b7dc6b8041f00dc
Snake7345 <thebasketman7@hotmail.com>
2018-02-16 20:33:47 +0100

    Mise a jour du Form
    Mise a jour du Form pour le formulaire de la série 6 exercice 2

---
+++ 6-02et09022017serie6/Correction/Exercice 2/Form.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theba
+ * Date: 09/02/18
+ * Time: :14
+ */
+
+class Form
+{
+    private this->monForm = '';
+
+    public function __construct($name, $ID, $methode, $action)
+    {
+        this->monForm='<form name="'.$name.'" method="'.$methode.'" action="'.$action.'" id="'.$ID.'">';
+    }
+
+    public function SetText($label, $name, $ID, $required, $placeholder, $value)
+    {
+        this->monForm.='<label for="'.$ID.'">'.$label.' </label>';
+		this->monForm.='<input type="text" name="'.$name.'" id="'.$ID.'"required="'.$required.'"
+		placeholder="'.$placeholder.'"value="'.$value.'"/><br/>';
+		this->monForm.='<br/>';
+    }
+
+    public function setEmail($label, $name, $ID, $required, $placeholder, $value)
+    {
+        this->monForm.='<label for="'.$ID.'">'.$label.' </label>';
+		this->monForm.='<input type="text" name="'.$name.'" id="'.$ID.'"required="'.$required.'"
+		placeholder="'.$placeholder.'"value="'.$value.'"/><br/>';
+		this->monForm.='<br/>';
+    }
+
+    public function setSubmit($name, $value)
+    {
+        this->monForm.='<br/><input type="submit" name="'.$name.'" value="'.$value.'"/>';
+    }
+
+    public function getForm($name, $value)
+    {
+
+    }
+}
---
+++ 6-02et09022017serie6/Correction/Exercice 2/page6.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theba
+ * Date: 09/02/18
+ * Time: 19:56
+ */
+
+$soustitre = "Personnage";
+require "head.php" ;
+?>
+    <div class="container-fluid">
+        <div class="row">
+            <header id="header" class="col-lg-10 offset-3">
+                <h1>Serie 6 : les classes</h1>
+            </header>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <section class="col-lg-10">
+                <div class="row">
+                    <article class="col-sm-10">
+
+                        <h2>Exercice 1</h2>
+                        <p>Combattez</p>
+
+                        <?php
+                        $perso1= new Personnage(50,5);
+                        $perso2= new Personnage(100,23);
+                        $perso1->frapper($perso2);
+                        $perso1->gagnerExperience();
+                        $perso2->frapper($perso1);
+                        $perso2->gagnerExperience();
+
+
+                        echo 'Le personnage 1 a ', $perso1->getforce(), ' de force, contrairement au personnage 2 qui a ', $perso2->getforce(), ' de force.<br />';
+
+                        echo 'Le personnage 1 a ', $perso1->getexperience(), ' d\'expérience, contrairement au personnage 2 qui a ', $perso2->getexperience(), ' d\'expérience.<br />';
+
+                        echo 'Le personnage 1 a ', $perso1->getdegats(), ' de dégâts, contrairement au personnage 2 qui a ', $perso2->getdegats(), ' de dégâts.<br />';
+
+                        ?>
+
+
+                    </article>
+
+                    <article class="col-sm-10">
+
+                        <h2>Exercice 2</h2>
+                        <p>Formulaire</p>
+
+                        <?php
+
+                        $Form1 = new Form(Formulaire, )
+
+                        ?>
+
+                    </article>
+
+                </div>
+            </section>
+            <aside class="col-lg-2">
+                <p>Contenu publicitaire</p>
+            </aside>
+
+
+        </div>
+    </div>
+<?php
+require 'bas.php' ;
+?>

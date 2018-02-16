28de813733f926ab6a172b13b4f20b92d1a0771c
Snake7345 <thebasketman7@hotmail.com>
2018-02-16 18:52:36 +0100

    Mise a jour correction
    Mise a jour Série 6 exercice 1

---
+++ 6-02et09022017serie6/Correction/Exercice 1/Form.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theba
+ * Date: 09/02/18
+ * Time: 20:14
+ */
+
+class Form
+{
+    private this->monForm = '';
+
+    public function Insta($Name, $ID, $methode, $action)
+    {
+
+    }
+
+    public function SetText($label, $name, $ID, $required, $placeholder, $value)
+    {
+
+    }
+
+    public function setEmail($label, $name, $ID, $required, $placeholder, $value)
+    {
+
+    }
+
+    public function setSubmit($name, $value)
+    {
+
+    }
+
+    public function getForm($name, $value)
+    {
+
+    }
+}
---
+++ 6-02et09022017serie6/Correction/Exercice 1/bas.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 02/02/18
+ * Time: 18:21
+ */
+?>
+<div class="container-fluid">
+    <div class="row">
+        <footer class="col-lg-12">
+            Fait par Bauduin Axel
+            <a href="#haut" >haut de page</a>
+        </footer>
+    </div>
+
+</div>
+</body>
+</html>

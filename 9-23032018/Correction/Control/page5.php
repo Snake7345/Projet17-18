fbbaa5db08a5aa021d497b6e599e4230e4c37b2e
Snake7345 <thebasketman7@hotmail.com>
2018-03-30 18:15:22 +0200

    Mise a jour fichier
    Mise a jour fichier

---
+++ 9-23032018/Correction/Control/page5.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theba
+ * Date: 09/03/18
+ * Time: 19:32
+ */
+?>
+
+<?php
+$soustitre= "Formulaire";
+require_once "../Vue/head.php";
+require_once "../Vue/page5.php";
+require_once "../Vue/bas.php";
+
+?>

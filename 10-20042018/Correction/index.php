674f2490c362506e53eb4f599011822718a4cbbd
Snake7345 <thebasketman7@hotmail.com>
2018-05-18 21:04:13 +0200

    Mise a jour
    Mise a jour mineure index mis hors des dossiers

--- 10-20042018/Correction/index.php
+++ 10-20042018/Correction/index.php
@@ -1,5 +1,15 @@
 <?php
 
+
 header('Location: control/page1.php');
+/*
+	require "core.php";
+	require "../Vue/head.php";
+	echo 'index.php';
+	$Tabletest=Model::load("tabletest");
+	$Tabletest->read();
+	require '../vue/index.php';
+	require '../vue/bas.php';
+*/
 
 ?>
